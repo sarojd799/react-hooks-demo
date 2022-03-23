ba4e129db401dbb479b8ced121c16e01b0d70d31
sarojd799 <saroj.dash799@gmail.com>
2022-03-23 10:04:29 +0530

    updated with header and footer and hooks module

--- hooks-demo/src/components/modules/programs/conversion/index.js
+++ hooks-demo/src/components/modules/programs/conversion/index.js
@@ -7,24 +7,27 @@ function Conversion(props) {
 
     return (
         <div className="conversion-container">
-            <div className="from-type">
-                <select>
-                    <option selected hidden disabled>FROM</option>
-                    <option>BINARY</option>
-                    <option>OCTAL</option>
-                    <option>DECIMAL</option>
-                    <option>HEX</option>
-                </select>
+            <div className="types-container">
+                <div className="from-type">
+                    <select>
+                        <option selected hidden disabled>FROM</option>
+                        <option>BINARY</option>
+                        <option>OCTAL</option>
+                        <option>DECIMAL</option>
+                        <option>HEX</option>
+                    </select>
+                </div>
+                <div className="to-type">
+                    <select>
+                        <option selected hidden disabled>TO</option>
+                        <option>BINARY</option>
+                        <option>OCTAL</option>
+                        <option>DECIMAL</option>
+                        <option>HEX</option>
+                    </select>
+                </div>
             </div>
-            <div className="to-type">
-                <select>
-                    <option selected hidden disabled>TO</option>
-                    <option>BINARY</option>
-                    <option>OCTAL</option>
-                    <option>DECIMAL</option>
-                    <option>HEX</option>
-                </select>
-            </div>
+
             <div className="inputs">
                 <input type="text" ref={inputA} />
                 <input type="text" ref={inputB} readOnly />
